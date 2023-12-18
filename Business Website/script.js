2e43c43bb9740a83b90335812ae4475b522eb45f
andrijanasharkoskaaandrijanasharkoskaa <andrijana.sh@gmail.com>
2023-12-19 00:47:22 +0100

    Changes

---
+++ Business Website/script.js
@@ -0,0 +1,11 @@
+function display() {
+  const cart = document.getElementsByClassName("your-cart");
+
+  cart.classList.add("your-cart");
+}
+
+function hide() {
+  const cart = document.getElementsByClassName("your-cart");
+
+  cart.classList.remove("your-cart");
+}
---
+++ Lesson 1 - Static Business Website/script.js
@@ -0,0 +1,5 @@
+$("#toabout").click(function () {
+  $("html,body").animate({
+    scrollTop: $(".info-section").offset(),
+  });
+});
