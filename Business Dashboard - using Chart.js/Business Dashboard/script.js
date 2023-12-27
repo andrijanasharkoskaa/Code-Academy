79923dba857bb788cb1b3c170ff657faf33a118f
andrijanasharkoskaaandrijanasharkoskaa <andrijana.sh@gmail.com>
2023-12-27 23:49:16 +0100

    commit

---
+++ Business Dashboard - using Chart.js/Business Dashboard/script.js
@@ -0,0 +1,35 @@
+const ctx = document.getElementById("myChart");
+
+new Chart(ctx, {
+  type: "bar",
+  data: {
+    labels: [
+      "01",
+      "02",
+      "03",
+      "04",
+      "05",
+      "06",
+      "07",
+      "08",
+      "09",
+      "10",
+      "11",
+      "12",
+    ],
+    datasets: [
+      {
+        label: "# of Votes",
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+});
