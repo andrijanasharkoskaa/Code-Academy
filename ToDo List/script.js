3f1b0443e34e0a0932f08b00510e95033f5707db
andrijanasharkoskaaandrijanasharkoskaa <andrijana.sh@gmail.com>
2024-01-22 23:36:55 +0100

    adding styling

--- ToDo List/script.js
+++ ToDo List/script.js
@@ -4,14 +4,15 @@ const xButton = document.querySelectorAll(".fa-xmark");
 
 btn.addEventListener("click", function () {
   const newItem = document.createElement("li");
-  newItem.innerHTML = "New Item" + `<i class="fa-solid fa-xmark"></i>`;
+  newItem.innerHTML =
+    "New Item" + `<i class="fa-solid fa-xmark" onclick="remove(this)"></i>`;
 
   list.append(newItem);
 });
 
 function remove(button) {
-  var listItem = button.parentNode;
+  const listItem = button.parentNode;
 
-  var todoList = document.getElementById("list");
+  const todoList = document.getElementById("list");
   todoList.removeChild(listItem);
 }
