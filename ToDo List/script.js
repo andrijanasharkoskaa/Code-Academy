435bab7095abb1e9b737e12074cf86d5d7f54f02
andrijanasharkoskaaandrijanasharkoskaa <andrijana.sh@gmail.com>
2024-01-22 23:49:45 +0100

    adding an array with items and iterating over it

--- ToDo List/script.js
+++ ToDo List/script.js
@@ -2,12 +2,23 @@ const list = document.getElementById("list");
 const btn = document.getElementById("btn");
 const xButton = document.querySelectorAll(".fa-xmark");
 
-btn.addEventListener("click", function () {
-  const newItem = document.createElement("li");
-  newItem.innerHTML =
-    "New Item" + `<i class="fa-solid fa-xmark" onclick="remove(this)"></i>`;
+const toDoItems = [
+  "Cook",
+  "Work",
+  "Clean",
+  "Wash my car",
+  "Study",
+  "Plan for the upcoming trip",
+];
+let currentIndex = 0;
 
-  list.append(newItem);
+btn.addEventListener("click", function () {
+  if (currentIndex < toDoItems.length) {
+    const newItem = document.createElement("li");
+    newItem.innerHTML = `${toDoItems[currentIndex]} <i class="fa-solid fa-xmark" onclick="remove(this)"></i>`;
+    list.appendChild(newItem);
+    currentIndex++;
+  }
 });
 
 function remove(button) {
