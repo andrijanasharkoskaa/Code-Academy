c96ce3579b3ace0490001573efeb75ae3b8807d2
andrijanasharkoskaaandrijanasharkoskaa <andrijana.sh@gmail.com>
2024-01-22 23:31:53 +0100

    fixing script

--- ToDo List/script.js
+++ ToDo List/script.js
@@ -1,9 +1,17 @@
 const list = document.getElementById("list");
 const btn = document.getElementById("btn");
+const xButton = document.querySelectorAll(".fa-xmark");
 
 btn.addEventListener("click", function () {
   const newItem = document.createElement("li");
-  newItem.innerHTML = "New Item";
+  newItem.innerHTML = "New Item" + `<i class="fa-solid fa-xmark"></i>`;
 
   list.append(newItem);
 });
+
+function remove(button) {
+  var listItem = button.parentNode;
+
+  var todoList = document.getElementById("list");
+  todoList.removeChild(listItem);
+}
