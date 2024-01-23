1097e8ece07307570e3ff4477e4ba5ed5a434aa8
andrijanasharkoskaaandrijanasharkoskaa <andrijana.sh@gmail.com>
2024-01-23 23:30:55 +0100

    adjusting scripts

---
+++ JS Exercise/dom.js
@@ -0,0 +1,5 @@
+const children = document.getElementsByClassName("child");
+const children2 = document.querySelectorAll("child");
+console.log(children, children2);
+
+function addNewChild() {}
--- ToDo List/script.js
+++ ToDo List/script.js
@@ -1,29 +1,38 @@
 const list = document.getElementById("list");
 const btn = document.getElementById("btn");
-const xButton = document.querySelectorAll(".fa-xmark");
+// const xButton = document.querySelectorAll(".fa-xmark");
 
-const toDoItems = [
-  "Cook",
-  "Work",
-  "Clean",
-  "Wash my car",
-  "Study",
-  "Plan for the upcoming trip",
-];
-let currentIndex = 0;
+// const toDoItems = [
+//   "Cook",
+//   "Work",
+//   "Clean",
+//   "Wash my car",
+//   "Study",
+//   "Plan for the upcoming trip",
+// ];
 
-btn.addEventListener("click", function () {
-  if (currentIndex < toDoItems.length) {
-    const newItem = document.createElement("li");
-    newItem.innerHTML = `${toDoItems[currentIndex]} <i class="fa-solid fa-xmark" onclick="remove(this)"></i>`;
-    list.appendChild(newItem);
-    currentIndex++;
-  }
-});
+// let currentIndex = 0;
 
-function remove(button) {
-  const listItem = button.parentNode;
+// btn.addEventListener("click", function () {
+//   if (currentIndex < toDoItems.length) {
+//     const newItem = document.createElement("li");
+//     newItem.innerHTML = `${toDoItems[currentIndex]} <i class="fa-solid fa-xmark" onclick="remove(this)"></i>`;
+//     list.appendChild(newItem);
+//     currentIndex++;
+//   }
+// });
 
-  const todoList = document.getElementById("list");
-  todoList.removeChild(listItem);
+// function remove(button) {
+//   const listItem = button.parentNode;
+//   list.removeChild(listItem);
+// }
+const userInput = document.getElementById("user-input");
+const parent = document.querySelector(".todo");
+
+btn.addEventListener("click", fun);
+
+function fun() {
+  const newItem = document.createElement("li");
+  newItem.innerHTML = userInput.value;
+  list.appendChild(newItem);
 }
