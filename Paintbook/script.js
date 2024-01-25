b4d9d05249e3ee3e6e3e22575e07d6ac3566c197
andrijanasharkoskaaandrijanasharkoskaa <andrijana.sh@gmail.com>
2024-01-25 22:59:25 +0100

    handling the click event to the Painting Book divs

--- Paintbook/script.js
+++ Paintbook/script.js
@@ -1,8 +1,15 @@
 const colors = document.querySelector(".colors");
 const colorDivs = document.querySelectorAll(".color-div");
+const paintDivs = document.querySelectorAll(".paint-div");
 
 colorDivs.forEach((colorDiv) => {
   colorDiv.addEventListener("click", () => {
     console.log("a div was clicked");
+  });
+});
+
+paintDivs.forEach((paintDiv) => {
+  paintDiv.addEventListener("click", () => {
+    console.log(" a TO-BE painted div is clicked");
   });
 });
