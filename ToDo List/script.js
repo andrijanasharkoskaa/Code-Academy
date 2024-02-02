7bd3ed5da9b9d63d16585f6cdaeae7d0ae7aba45
andrijanasharkoskaaandrijanasharkoskaa <andrijana.sh@gmail.com>
2024-02-02 13:22:00 +0100

    working on adding items functionality

--- ToDo List/script.js
+++ ToDo List/script.js
@@ -2,40 +2,18 @@ const list = document.getElementById("list");
 const btn = document.getElementById("btn");
 // const xButton = document.querySelectorAll(".fa-xmark");
 
-// const toDoItems = [
-//   "Cook",
-//   "Work",
-//   "Clean",
-//   "Wash my car",
-//   "Study",
-//   "Plan for the upcoming trip",
-// ];
-
-// let currentIndex = 0;
-
-// btn.addEventListener("click", function () {
-//   if (currentIndex < toDoItems.length) {
-//     const newItem = document.createElement("li");
-//     newItem.innerHTML = `${toDoItems[currentIndex]} <i class="fa-solid fa-xmark" onclick="remove(this)"></i>`;
-//     list.appendChild(newItem);
-//     currentIndex++;
-//   }
-// });
-
-// function remove(button) {
-//   const listItem = button.parentNode;
-//   list.removeChild(listItem);
-// }
 const userInput = document.getElementById("user-input");
+
 const parent = document.querySelector(".todo");
 
-btn.addEventListener("click", fun);
-
-function fun() {
+function addToDoItem() {
   const newItem = document.createElement("li");
-  newItem.innerHTML = userInput.value;
+  newItem.classList.add("list-item");
+  newItem.innerHTML = `${userInput.value} <i class="fa-solid fa-xmark" onclick="removeItem()"></i> <i class="fa-solid fa-pencil" onclick="editItem()"></i>`;
   list.appendChild(newItem);
+  userInput.value = "";
 }
+btn.addEventListener("click", addToDoItem);
 
-const secondList = document.getElementById("second-list");
-console.log(secondList.children);
+function removeItem() {}
+function editItem() {}
