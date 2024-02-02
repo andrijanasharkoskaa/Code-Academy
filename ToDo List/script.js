186f1cace6cb8c969abf6259f0ba29d4f4a6432b
andrijanasharkoskaaandrijanasharkoskaa <andrijana.sh@gmail.com>
2024-02-02 13:36:47 +0100

    working with localStorage

--- ToDo List/script.js
+++ ToDo List/script.js
@@ -9,6 +9,7 @@ const parent = document.querySelector(".todo");
 function addToDoItem() {
   const newItem = document.createElement("li");
   newItem.classList.add("list-item");
+  newItem.id = "list-item";
   newItem.innerHTML = `${userInput.value} <i class="fa-solid fa-xmark" onclick="removeItem()"></i> <i class="fa-solid fa-pencil" onclick="editItem()"></i>`;
   list.appendChild(newItem);
   userInput.value = "";
@@ -17,3 +18,24 @@ btn.addEventListener("click", addToDoItem);
 
 function removeItem() {}
 function editItem() {}
+// const todos = [{ id: 0, title: "hello" }];
+// localStorage.setItem("item", JSON.stringify(todos));
+
+// var storedTodos = JSON.parse(localStorage.getItem("item"));
+// localStorage.clear();
+
+console.log(storedTodos);
+
+window.onload = function () {
+  // Check for LocalStorage support.
+  if (localStorage) {
+    // Add an event listener for form submissions
+    document.getElementById("list").addEventListener("submit", function () {
+      // Get the value of the name field.
+      var item = document.getElementById("list-item").value;
+
+      // Save the name in localStorage.
+      localStorage.setItem("item", item);
+    });
+  }
+};
