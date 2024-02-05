b7d71925c0ba260985449497cd2b085d11ce2f1e
andrijanasharkoskaaandrijanasharkoskaa <andrijana.sh@gmail.com>
2024-02-05 16:58:12 +0100

    working on styling

--- Chatbot/script.js
+++ Chatbot/script.js
@@ -1,6 +1,7 @@
 let messageEl = document.getElementById("messages");
 let chatBtn = document.getElementById("chat-btn");
 let askInput = document.getElementById("ask");
+let chat = document.getElementById("chat");
 
 let chatMessages = [
   ["Today is Monday"],
@@ -10,22 +11,30 @@ let chatMessages = [
 ];
 
 function askQuestion() {
-  "";
-
-  // Display user input
   let userInput = document.createElement("li");
+  userInput.classList.add("response", "incoming");
   userInput.innerText = askInput.value.toLowerCase();
   messageEl.appendChild(userInput);
 
-  // Display messages that include the keyword specified by the user
   setTimeout(() => {
     const keyword = askInput.value.toLowerCase();
     chatMessages.forEach((message) => {
       if (message[0].toLowerCase().includes(keyword)) {
         let response = document.createElement("li");
+        response.classList.add("response", "outgoing");
         response.innerText = message[0];
         messageEl.appendChild(response);
       }
     });
   }, 1500);
+}
+let close = document.getElementById("close");
+function closeChat() {
+  chat.style.display = "none";
+  let exitIcon = document.getElementsById("exit-icon");
+  exitIcon.innerHTML = "";
+}
+function openChat() {
+  chat.style.display = "block";
+  closes.style.display = "none";
 }
