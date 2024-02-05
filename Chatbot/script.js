3e471275b148a6678ada62fb4d6e002f6f7b7719
andrijanasharkoskaaandrijanasharkoskaa <andrijana.sh@gmail.com>
2024-02-05 16:24:01 +0100

    fixing the chat functionality

--- Chatbot/script.js
+++ Chatbot/script.js
@@ -5,18 +5,23 @@ let askInput = document.getElementById("ask");
 let chatMessages = [
   ["Today is Monday"],
   [
-    "Front-end programming requires knowledge of HTML, CSS, JavaScript, React or Bootstra",
+    "Front-end programming requires knowledge of HTML, CSS, JavaScript, React or Bootstrap",
   ],
 ];
 
 function askQuestion() {
-  messageEl.innerHTML = "";
+  "";
+
+  // Display user input
   let userInput = document.createElement("li");
   userInput.innerText = askInput.value.toLowerCase();
   messageEl.appendChild(userInput);
+
+  // Display messages that include the keyword specified by the user
   setTimeout(() => {
+    const keyword = askInput.value.toLowerCase();
     chatMessages.forEach((message) => {
-      if (askInput.value.toLowerCase().includes("today")) {
+      if (message[0].toLowerCase().includes(keyword)) {
         let response = document.createElement("li");
         response.innerText = message[0];
         messageEl.appendChild(response);
@@ -24,5 +29,3 @@ function askQuestion() {
     });
   }, 1500);
 }
-
-// function appendEl(message) {}
