35da3ec1e4ca69a385dd11eb41813fee7097a015
andrijanasharkoskaaandrijanasharkoskaa <andrijana.sh@gmail.com>
2024-02-05 16:18:03 +0100

    implementing chat functionality

--- Chatbot/script.js
+++ Chatbot/script.js
@@ -1,18 +1,28 @@
 let messageEl = document.getElementById("messages");
 let chatBtn = document.getElementById("chat-btn");
-let askInput = document.getElementById("ask").value;
+let askInput = document.getElementById("ask");
 
 let chatMessages = [
-  "Today is Monday",
-  "Front-end programming requires knowledge of HTML, CSS, JavaScript, React or Bootstra",
+  ["Today is Monday"],
+  [
+    "Front-end programming requires knowledge of HTML, CSS, JavaScript, React or Bootstra",
+  ],
 ];
 
-function askChatbot() {
-  chatMessages.forEach((message) => {
-    if (askInput.includes("front-end programming")) {
-      let response = document.createElement("li");
-      response.innerText = message[1];
-      messageEl.appendChild(response);
-    }
-  });
+function askQuestion() {
+  messageEl.innerHTML = "";
+  let userInput = document.createElement("li");
+  userInput.innerText = askInput.value.toLowerCase();
+  messageEl.appendChild(userInput);
+  setTimeout(() => {
+    chatMessages.forEach((message) => {
+      if (askInput.value.toLowerCase().includes("today")) {
+        let response = document.createElement("li");
+        response.innerText = message[0];
+        messageEl.appendChild(response);
+      }
+    });
+  }, 1500);
 }
+
+// function appendEl(message) {}
