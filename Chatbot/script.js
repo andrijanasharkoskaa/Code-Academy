91c7a4868a2b6e1b1b13cb4f98de2efdb690a485
andrijanasharkoskaaandrijanasharkoskaa <andrijana.sh@gmail.com>
2024-02-05 23:38:05 +0100

    fixing the closeChat() functionality

--- Chatbot/script.js
+++ Chatbot/script.js
@@ -1,6 +1,11 @@
 let chatboxIcon = document.getElementById("chatbox-icon");
 let chatbotContainer = document.getElementById("chatbot-container");
+let exitBtn = document.getElementById("exit-header");
 
 function openChat() {
   chatbotContainer.classList.toggle("display-class-none");
+}
+
+function closeChat() {
+  chatbotContainer.classList.add("display-class-none");
 }
