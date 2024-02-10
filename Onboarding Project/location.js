5ce2acace5e1116832bbbcaf2bf54d736a2f3083
andrijanasharkoskaaandrijanasharkoskaa <andrijana.sh@gmail.com>
2024-02-10 14:34:47 +0100

    adding the correct gif

--- Onboarding Project/location.js
+++ Onboarding Project/location.js
@@ -1,6 +1,6 @@
 window.onload = function () {
   let rightSection = document.getElementById("right-section");
   if (window.location.href.includes("location.html")) {
-    rightSection.style.backgroundImage = "url('assets/second-slide.gif')";
+    rightSection.style.backgroundImage = "url('assets/city.gif')";
   }
 };
