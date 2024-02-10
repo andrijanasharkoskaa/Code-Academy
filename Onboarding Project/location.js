c4f349e070ff084ce3a171f8bd15e081bbdf1b2b
andrijanasharkoskaaandrijanasharkoskaa <andrijana.sh@gmail.com>
2024-02-10 14:57:50 +0100

    adding the range site and functionality for the search and range inputs

--- Onboarding Project/location.js
+++ Onboarding Project/location.js
@@ -2,5 +2,13 @@ window.onload = function () {
   let rightSection = document.getElementById("right-section");
   if (window.location.href.includes("location.html")) {
     rightSection.style.backgroundImage = "url('assets/city.gif')";
+  } else if (window.location.href.includes("hourly-rate.html")) {
+    rightSection.style.backgroundImage = "url('assets/hourly-rate.gif')";
   }
 };
+
+let searchBtn = document.getElementById("search-btn");
+
+searchBtn.addEventListener("click", () => {
+  windows.location.href = "hourly-rate.html";
+});
