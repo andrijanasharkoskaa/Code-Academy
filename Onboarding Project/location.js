c50534370f7096e4982e226dc2696f539b29a6c4
andrijanasharkoskaaandrijanasharkoskaa <andrijana.sh@gmail.com>
2024-02-16 02:09:10 +0100

    utilizing API for retrieving locations for my search functionality

--- Onboarding Project/location.js
+++ Onboarding Project/location.js
@@ -14,8 +14,27 @@ searchBtn.addEventListener("click", () => {
   windows.location.href = "hourly-rate.html";
 });
 
-function updateOutput(input) {
-  document.getElementById(
-    input.id.replace("price", "range-value")
-  ).textContent = input.value;
+const url =
+  "https://countryapi.io/api/all?apikey=bvZGgLNMFo7Htrbeg9NWhEDju7O7iEGDZI8ffQZx";
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "bvZGgLNMFo7Htrbeg9NWhEDju7O7iEGDZI8ffQZx",
+    Authorization: "Bearer bvZGgLNMFo7Htrbeg9NWhEDju7O7iEGDZI8ffQZx",
+  },
+};
+
+async function fetchData() {
+  try {
+    const response = await fetch(url, options);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const result = await response.json();
+    console.log(result);
+  } catch (error) {
+    console.error(error);
+  }
 }
+
+fetchData();
---
+++ Onboarding Project/specialization.js
@@ -0,0 +1,1 @@
+const roles = document.getElementById("roles");
